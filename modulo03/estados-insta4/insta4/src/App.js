00dee7b8bd3f080c6f86dc5253e3af4029150407
Antonio Cláudio Holanda Borges Junior <claudiohbjunior@gmail.com>
2022-07-05 21:44:35 -0300

    Entrega do exercicio de renderização

--- modulo03/estados-insta4/insta4/src/App.js
+++ modulo03/estados-insta4/insta4/src/App.js
@@ -4,24 +4,30 @@ import './style.css'
 
 
 function App() {
+  const arrayDeInformacoes =[ {nomeUsuario:"paulinha", fotoUsuario:"https://picsum.photos/50/50", fotoPost:'https://picsum.photos/200/150'},
+                              {nomeUsuario:"samynha", fotoUsuario:"https://picsum.photos/50/49", fotoPost:'https://picsum.photos/200/151'},
+                              {nomeUsuario:"julinha", fotoUsuario:"https://picsum.photos/50/58", fotoPost:'https://picsum.photos/200/152'}
+
+]
+
+const arrayDeLista = arrayDeInformacoes.map((elemento, index) => {
+  return <Post
+  nomeUsuario={elemento.nomeUsuario}
+  fotoUsuario={elemento.fotoUsuario}
+  fotoPost={elemento.fotoPost}
+
+  key={index}
+  />
+  
+
+})
 return(
   <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-                     <Post
-            nomeUsuario={'samynha'}
-            fotoUsuario={'https://picsum.photos/50/49'}
-            fotoPost={'https://picsum.photos/200/151'}
-          />
-                     <Post
-            nomeUsuario={'julinha'}
-            fotoUsuario={'https://picsum.photos/50/58'}
-            fotoPost={'https://picsum.photos/200/152'}
-          />
+    {arrayDeLista}
+
+    {console.log(arrayDeInformacoes)}
   </div>
+  
   
   
 )
