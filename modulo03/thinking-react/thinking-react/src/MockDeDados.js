92dd154d2468551b1a1c9627ba3a167768999ccb
Antonio Cláudio Holanda Borges Junior <claudiohbjunior@gmail.com>
2022-07-14 23:24:28 -0300

    Entrega do exercicio thinking-react

---
+++ modulo03/thinking-react/thinking-react/src/MockDeDados.js
@@ -0,0 +1,6 @@
+export const dados = [
+    {id:"",texto:"Tarefa1",status:false},
+    {id:"",texto:"Tarefa2",status:false},
+    {id:"",texto:"Tarefa3",status:false},
+    {id:"",texto:"Tarefa4",status:false},
+]
