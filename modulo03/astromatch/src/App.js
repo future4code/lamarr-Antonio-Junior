a1493c99dd5674727c258633074813dff183e1e0
Antonio Cláudio Holanda Borges Junior <claudiohbjunior@gmail.com>
2022-08-07 21:49:00 -0300

    Engrega final2 do projeto Astromatch

--- modulo03/astromatch/src/App.js
+++ modulo03/astromatch/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import TelaDeMatches from './Components/TelaDeMatches/TelaDeMatches';
 import TelaInicial from './Components/TelaInicial/TelaInicial';
+import { AiFillHome } from 'react-icons/ai';
 import {
   BrowserRouter as Router,
   Link,
@@ -11,8 +12,11 @@ import Header from './Components/Header/Header';
 
 
 function App() {
+
   return (
     <Router>
+      
+      <Link to={"/telaInicial"}><AiFillHome className='home' /></Link>
     <div className="App">
       <Routes>
       <Route path="/matches" exact element={<TelaDeMatches />} />
